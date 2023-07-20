e60ab5bb80b55ffb92ea5d3a005faea7268e6aa1
Archibald Macharia Gacheru <archibald.gacheru@student.moringaschool.com>
2023-07-20 20:17:43 +0300

    Pac 3 functions complete

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+}
+console.log(introduction("Aki"));
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguage("Gracie","JavaScript");
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguageOptional("Macharia");
